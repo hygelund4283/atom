1046950ed82a36b510b0206a775a6045ddbd35fe
hygelund4283 <hygelund4283@gmail.com>
2017-10-15 16:19:13 -0700

    first commit

--- components/Movie.js
+++ components/Movie.js
@@ -1,33 +1,29 @@
 import React from 'react';
-import { Container, Header, Content, Badge, Text } from 'native-base';
+import { Container, Header, Content, List, ListItem, Text } from 'native-base';
+
 
 const Movie = () => {
   return (
       <Container>
         <Header />
         <Content>
-          <Badge>
-            <Text>2</Text>
-          </Badge>
-          <Badge primary>
-            <Text>2</Text>
-          </Badge>
-          <Badge success>
-            <Text>2</Text>
-          </Badge>
-          <Badge info>
-            <Text>2</Text>
-          </Badge>
-          <Badge warning>
-            <Text>2</Text>
-          </Badge>
-          <Badge danger>
-            <Text>2</Text>
-          </Badge>
-          
-          <Badge style={{ backgroundColor: 'black' }}>
-            <Text style={{ color: 'white' }}>1866</Text>
-          </Badge>
+          <List>
+            <ListItem itemDivider>
+              <Text>A</Text>
+            </ListItem>
+            <ListItem >
+              <Text>Aaron Bennet</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Ali Connors</Text>
+            </ListItem>
+            <ListItem itemDivider>
+              <Text>B</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Bradley Horowitz</Text>
+            </ListItem>
+          </List>
         </Content>
       </Container>
   )
